99be9a6138948a3ef1ddb1a01817313151c262c5
killthebee <51859323+killthebee@users.noreply.github.com>
2020-01-13 01:12:33 +0300

    renamed vars and newer parser

--- text_file_parser.py
+++ text_file_parser.py
@@ -2,11 +2,14 @@ import re
 
 
 def parse_item(item):
-    first_index = re.search('[:] ', item).end()
-    return item[first_index:]
+    try:
+        first_index = re.search('[:] ', item).end()
+        return item[first_index:]
+    except AttributeError:
+        return None
 
 
-def parse_drinks_type(item):
+def separate_type_and_units(item):
 
     drink_type_last_index = re.search('\n\n', item).end() - 2
     if item.startswith('#'):
@@ -14,61 +17,51 @@ def parse_drinks_type(item):
         drink_type = item[first_index:drink_type_last_index]
     else:
         drink_type = item[:drink_type_last_index]
-    return drink_type
+    units_first_index = drink_type_last_index + 3
+    return drink_type, item[units_first_index:]
 
 
-def parse_drinks_info(item):
-
-    second_split = item.split('\n')
-    drinks_info = []
-    for index, item in enumerate(second_split, 0):
-        if item.startswith('Название'):
-
-            variety_index = index + 1
-            price_index = index + 2
-            picture_index = index + 3
-            is_profitable_index = index + 4
-
-            name = parse_item(item)
-            try:
-                variety = parse_item(second_split[variety_index])
-            except AttributeError:
-                variety = None
-            price = parse_item(second_split[price_index])
-            picture = parse_item(second_split[picture_index])
-            try:
-                if second_split[is_profitable_index].startswith('Выгодное'):
-                    is_profitable = True
-                else:
-                    is_profitable = None
-            except IndexError:
-                is_profitable = None
-
-            drink_info = {
-                'name': name,
-                'variety': variety,
-                'price': price,
-                'picture': picture,
-                'is_profitable': is_profitable,
-            }
-
-            drinks_info.append(drink_info)
-
-    return drinks_info
+def parse_drinks(items):
+    specifications = items.split('\n')
+    if 'Выгодное предложение' in specifications:
+        is_profitable = True
+    else:
+        is_profitable = False
+    for specification in specifications:
+        if specification.startswith('Название:'):
+            name = parse_item(specification)
+        elif specification.startswith('Сорт:'):
+            variety = parse_item(specification)
+        elif specification.startswith('Цена:'):
+            price = parse_item(specification)
+        elif specification.startswith('Картинка:'):
+            picture = parse_item(specification)
+    drink_specs = {
+        'name': name,
+        'variety': variety,
+        'price': price,
+        'picture': picture,
+        'is_profitable': is_profitable,
+    }
+    return drink_specs
 
 
 def fetch_drinks(file_name):
 
     with open(file_name, 'r', encoding='UTF-8') as file:
         text = file.read()
-
-    first_split = text.split('\n\n\n# ')
-
     drinks = []
-    for item in first_split:
-        drink = {}
-        drink['type'] = parse_drinks_type(item)
-        drink['info'] = parse_drinks_info(item)
-        drinks.append(drink)
-
+    split_text = text.split('\n\n\n# ')
+    for specific_type_drink in split_text:
+        drink_type_name, specific_type_drink_units = separate_type_and_units(specific_type_drink)
+        split_specific_type_drink_units = specific_type_drink_units.split('\n\n')
+        drink_info_list = []
+        for drink_unit_info in split_specific_type_drink_units:
+            parsed_drink_unit_info = parse_drinks(drink_unit_info)
+            drink_info_list.append(parsed_drink_unit_info)
+        drink_type = {
+            'type': drink_type_name,
+            'info': drink_info_list,
+        }
+        drinks.append(drink_type)
     return drinks
